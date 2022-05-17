2b05939242a1dd44845a4f3a1ce5f33802f1520c
Ankit yadav <ankityadav1738@gmail.com>
2022-05-17 14:44:20 +0100

    Add project files.

---
+++ funciton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace funciton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(12 , 18);
+            Console.WriteLine("Value of first object");
+            r1.display();
+
+            Rectangle r2 = new Rectangle(r1);
+            Console.WriteLine("Enter the value of second object");
+            r2.display();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ funciton/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace funciton
+{
+    class Rectangle
+    {
+        public int length;
+        public int width;
+
+        public Rectangle(int x, int y)
+        {
+            length = x;
+            width = y;
+        }
+        public Rectangle(Rectangle r)
+        {
+            length = r.length;
+            width = r.width;
+        }
+        public void display()
+        {
+            Console.WriteLine(" __________x_____________  ");
+            Console.WriteLine("|                        | ");
+            Console.WriteLine("|                        | ");
+            Console.WriteLine("|y                       | ");
+            Console.WriteLine("|                        | ");
+            Console.WriteLine("|________________________| ");
+
+            Console.WriteLine("Length:" + length);
+            Console.WriteLine("Width:" + width);
+
+            int perimeter = (length * 2) + (width * 2);
+
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("Area:" + length * width);
+            Console.WriteLine("Perimeter:" + perimeter);
+        }
+    }
+}
